b6895b94b586349c6e31a9f6db151273693d49e9
Evgenii Bukhgammer <bukhgammer@gmail.com>
2020-10-20 22:21:58 -0700

    initial travis ci config

--- skipping_group_of_parametrized_tests/tests/test_group.py
+++ skipping_group_of_parametrized_tests/tests/test_group.py
@@ -1,8 +1,11 @@
-import pytest
+from time import sleep
+from selenium import webdriver
 
 
-@pytest.mark.parametrize("a,b,result", [(1,2,3), (2,3,4), (3,3,7), (4,4,8)])
-def test_summarize(a, b, result, skip_list):
-    if 'test_summarize' in skip_list:
-        pytest.skip('skipped!')
-    assert sum([a, b]) == result
+def test_A():
+    browser = webdriver.Chrome()
+    browser.get('https://yandex.ru/')
+    browser.execute_script("alert('hello');")
+    sleep(2)
+    browser.switch_to.alert.accept()
+    sleep(10)
